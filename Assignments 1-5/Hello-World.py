946687ddcf331154f38e117bab4b77f72b6791a5
ArthurM2332 <mijares.arthur@greenriver.edu>
2025-01-12 15:44:43 -0800

    I have created Hello world script

---
+++ Assignments 1-5/Hello-World.py
@@ -0,0 +1,13 @@
+name = input("Insert Your Name here: ")
+
+print("Hello", name)
+
+print("today is going to be a great day.")
+
+age = input("Insert Your Age Here: ")
+
+age = int(age)
+
+print("Two years in the future you will be", age + 2)
+
+
