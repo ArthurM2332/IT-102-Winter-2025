c0f9c641d5f66fc2dfa018e9911a52d697b6bb58
ArthurM2332 <mijares.arthur@greenriver.edu>
2025-01-19 21:38:31 -0800

    Updated Conditional To a Loop Script

--- Assignments 1-5/Conditional.py
+++ Assignments 1-5/Conditional.py
@@ -1,7 +1,8 @@
 answer = input("Is today a good day? (y/n): ")
 
 if answer == 'y':
-    print("Yes it is")
+    for _ in range(10):
+        print("Yes it is")
 
 if answer == 'n':
     print ("Sorry to hear that hopefully tomorrow is a great day")
