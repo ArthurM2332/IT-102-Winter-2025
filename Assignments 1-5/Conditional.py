eec9db45307874cb0a663d1014a3c51ed44dc55e
ArthurM2332 <mijares.arthur@greenriver.edu>
2025-01-26 20:49:21 -0800

    Added Function

--- Assignments 1-5/Conditional.py
+++ Assignments 1-5/Conditional.py
@@ -1,8 +1,16 @@
+
+def send_message():
+    for _ in range(10):
+        print("Yes it is")
+
+
+
+
+
 answer = input("Is today a good day? (y/n): ")
 
 if answer == 'y':
-    for _ in range(10):
-        print("Yes it is")
+    send_message()
 
 if answer == 'n':
     print ("Sorry to hear that hopefully tomorrow is a great day")
