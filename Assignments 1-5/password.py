bd322728d3532011e7e2b55b2163c99ee3429e06
ArthurM2332 <mijares.arthur@greenriver.edu>
2025-02-09 22:44:17 -0800

    Created Password Generator

---
+++ Assignments 1-5/password.py
@@ -0,0 +1,40 @@
+
+
+from passlib.hash import sha512_crypt 
+SHADOW_FILE = r'/home/justincase/Desktop/IT-102-Winter-2025/Assignments 1-5/shadow.txt'
+PASSWORD_FILE = r'/home/justincase/Desktop/IT-102-Winter-2025/Assignments 1-5/passwords.txt'
+def guess_password(shadow_file, password_file):
+    successful_attempts = []
+    try:
+        with open(shadow_file, 'r', encoding='utf-8') as sf, open(password_file, 'r', encoding='utf-8') as pf:
+            shadows = sf.readlines()
+            passwords = [pw.strip() for pw in pf.readlines()]
+        
+        for shadow in shadows:
+            parts = shadow.split(":")
+            if len(parts) < 2 or '!' in parts[1] or '*' in parts[1]:
+                continue
+
+            username, hash_password = parts[0], parts[1].strip()
+
+            for password in passwords:
+                try:
+                    if sha512_crypt.verify(password, hash_password):
+                        successful_attempts.append((username, password))
+                        print(f"[+] Cracked {username}: {password}")
+                        
+                except ValueError:
+                        continue
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+        return
+
+    if successful_attempts:
+        print("\n=== CRACKED PASSWORDS ===")
+        for username, password in successful_attempts:
+            print(f"User: {username}, Password: {password}")
+    else:
+        print("\nNo passwords were found or cracked")
+
+# Run the function
+guess_password(SHADOW_FILE, PASSWORD_FILE)
