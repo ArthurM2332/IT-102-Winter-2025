8b7c217169b85ad60505f13bd91a4cca3d7e816b
ArthurM2332 <mijares.arthur@greenriver.edu>
2025-03-02 22:47:07 -0800

    ss

---
+++ Assignments 1-5/Regularexpr.py
@@ -0,0 +1,17 @@
+import re
+from collections import Counter 
+
+log_file_path = "/home/justincase/Desktop/IT-102-Winter-2025/Assignments 1-5/access.log"
+
+log_pattern = r'"[A-Z]+ [^"]+ HTTP/1\.[01]" (\d{3}'
+
+with open(log_file_path, "r") as file:
+    logs = file.readlines()
+
+status_codes = [re.search(log_pattern, line).group(1) for line in logs if re.search(log_pattern, line)]  
+
+status_counts= Counter(status_codes)
+
+print("HTTP Status Codes Distributions: ")
+for status, count in status_counts.items():
+    print(f"{status}: {count}")
